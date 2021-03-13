49d036f91d3220dcdc25ee7f64ac4bf551ccdc22
Tymoteusz Makowski <tymek.makowski@gmail.com>
2021-03-13 19:40:54 +0100

    Implement flipCross usage on click

--- js/game.js
+++ js/game.js
@@ -1,8 +1,22 @@
+var clicked = {
+  x: null,
+  y: null,
+}
+
 function create(g) {}
 
-function update(g) {}
+function update(g) {
+  if (clicked.x !== null && clicked.y !== null) {
+    flipCross(g, clicked.x, clicked.y);
+    clicked.x = null;
+    clicked.y = null;
+  }
+}
 
-function onDotClicked(x, y) {}
+function onDotClicked(x, y) {
+  clicked.x = x;
+  clicked.y = y;
+}
 
 let dim = {
   w: 5,
@@ -16,6 +30,7 @@ let config = {
   onDotClicked: onDotClicked,
   gridHeight: dim.h,
   gridWidth: dim.w,
+  clearGrid: false,
 }
 
 let game = new Game(config);
