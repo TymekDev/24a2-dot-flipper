5c03f189d473878fbdac0c6a0e783f84a666e442
Tymoteusz Makowski <tymek.makowski@gmail.com>
2021-03-14 01:26:42 +0100

    Add scramble_moves getter

--- js/game_master.js
+++ js/game_master.js
@@ -1,8 +1,19 @@
 let game_master = class {
-  width      = 5;
-  height     = 5;
+  width  = 5;
+  height = 5;
+
   color_main = Color.Gray;
   color_alt  = Color.Black;
+
+  scramble_min = 2;
+  scramble_max = 10;
+
+  get scramble_moves() {
+    return Math.max(
+      this.scramble_min,
+      Math.floor(Math.random() * this.scramble_max)
+    );
+  }
 
   clear_canvas(g) {
     for (let ix = 0; ix < this.width; ix++) {
